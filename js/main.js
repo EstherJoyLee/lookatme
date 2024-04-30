3aaf7d1544bdbf46ad2e6ba09c6d12ddda804197
Esther <bonjourjj3@gmail.com>
2024-04-30 12:20:25 +0900

    editing_portfolio

---
+++ js/main.js
@@ -0,0 +1,30 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const grid = new Isotope("section", {
+    itemSelector: "article",
+    columnWidth: "article", // itemSelector 와 동일한 값을 지정하면 자동으로 넓이 값을 계산해줌
+    transitionDuration: "0.5s",
+  }); // first param: parent of target elem
+
+  const btns = document.querySelectorAll("main ul li");
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const isOn = e.currentTarget.classList.contains("on");
+      if (!isOn) return;
+      activation(e);
+    });
+  });
+
+  const activation = function (e) {
+    for (let btn of btns) {
+      btn.classList.remove("on");
+    }
+    e.currentTarget.classList.add("on");
+    const btn_a = e.currentTarget.querySelector("a");
+    const a_href = btn_a.getAttribute("href");
+
+    grid.arrange({
+      filter: a_href,
+    });
+  };
+});
